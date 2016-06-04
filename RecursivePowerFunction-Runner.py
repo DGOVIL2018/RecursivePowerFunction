3f0a93332c3bddb4431038db1e3c427623402696
DGOVIL2018 <DGOVIL2018@pingry.org>
2016-06-03 22:37:55 -0400

    Delete RecursivePowerFunction-Runner.py

--- RecursivePowerFunction-Runner.py
+++
@@ -1,7 +0,0 @@
-import RecursivePowerFunction
-
-print(RecursivePowerFunction.power(5,2))
-print(RecursivePowerFunction.power(-5,3))
-print(RecursivePowerFunction.power(8,0))
-print(RecursivePowerFunction.power(5,-3))
-print(RecursivePowerFunction.power(0,8))
